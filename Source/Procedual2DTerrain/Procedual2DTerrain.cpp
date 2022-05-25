7a4254298ce69887fdb9c65c6ccebe183fb501f3
Liam Healey <snickerscattoruletheworld@gmail.com>
2022-05-25 12:24:15 -0600

    Initial commit

---
+++ Source/Procedual2DTerrain/Procedual2DTerrain.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Procedual2DTerrain.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Procedual2DTerrain, "Procedual2DTerrain" );
---
+++ Source/Procedual2DTerrain/Procedual2DTerrainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Procedual2DTerrainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDUAL2DTERRAIN_API AProcedual2DTerrainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
